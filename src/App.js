ad92b5d0002646b203e3e208dc00dca4d136670f
Patipat Padpong <patipat.developer@gmail.com>
2022-09-18 14:34:00 +0700

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,59 +1,58 @@
-import './App.css';
-import {useState} from 'react';
-import AppHeader from './components/AppHeader';
-import PicItem from './components/PicItem';
-import PicPost from './components/PicPost';
-import shohokuTeam from './data/shohokuTeam';
-import AppSearch from './components/AppSearch';
-
-
+import "./App.css";
+import { useState } from "react";
+import AppHeader from "./components/AppHeader";
+import PicItem from "./components/PicItem";
+import PicPost from "./components/PicPost";
+import shohokuTeam from "./data/shohokuTeam";
+import AppSearch from "./components/AppSearch";
 
 function App() {
+  const [selectedPic, setSelectedPic] = useState(null);
+  const [searchText, setSearchText] = useState("");
 
-   const [selectedPic,setSelectedPic] = useState(null);
-   const [searchText, setSearchText] = useState('');
+  function onPictureClick(openPicture) {
+    setSelectedPic(openPicture);
+  }
 
+  function PictureClose() {
+    setSelectedPic(null);
+  }
 
-   function onPictureClick(openPicture) {
-      setSelectedPic(openPicture);
-   }
+  // const fillteredPictures = shohokuTeam.filter((picture)=>{
+  //    return picture.title.includes(searchText);
+  // })
 
-   function PictureClose () {
-      setSelectedPic(null);
-   }
-
-   // const fillteredPictures = shohokuTeam.filter((picture)=>{
-   //    return picture.title.includes(searchText);
-   // })
-
-
-   const pictureLists = shohokuTeam.filter((picture)=>{
+  const pictureLists = shohokuTeam
+    .filter((picture) => {
       return picture.title.includes(searchText);
-   }).map((picture, index)=>{
-      return <PicItem key={index} picture={picture} openSlamDunkPic = {onPictureClick} />;
-   });
+    })
+    .map((picture, index) => {
+      return (
+        <PicItem
+          key={index}
+          picture={picture}
+          openSlamDunkPic={onPictureClick}
+        />
+      );
+    });
 
-   let slamdunkPost = null;
-   if(!!selectedPic) {
-      slamdunkPost = <PicPost picture={selectedPic} onBgClick={PictureClose}/>
-   }
+  let slamdunkPost = null;
+  if (!!selectedPic) {
+    slamdunkPost = <PicPost picture={selectedPic} onBgClick={PictureClose} />;
+  }
 
-
-   return (
-      <div className = "app">
-            <AppHeader/>
-            <section className='app-section'>
-               <div className='app-container'>
-               <AppSearch value={searchText} onValueChange={setSearchText} />
-               <div className="app-grid">
-                  {pictureLists}
-               </div>
-               </div>
-            </section>
-         {slamdunkPost}
-      </div>
-   );
+  return (
+    <div className="app">
+      <AppHeader />
+      <section className="app-section">
+        <div className="app-container">
+          <AppSearch value={searchText} onValueChange={setSearchText} />
+          <div className="app-grid">{pictureLists}</div>
+        </div>
+      </section>
+      {slamdunkPost}
+    </div>
+  );
 }
 
 export default App;
-
